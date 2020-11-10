11bf4ef0f6b384ef9c1935cb3a5a56b0078b4106
starvapour <53104323+starvapour@users.noreply.github.com>
2020-11-10 11:58:52 +0800

    Update reviewDetail.py

--- Sprint 1/filmFinder/reviewDetail.py
+++ Sprint 1/filmFinder/reviewDetail.py
@@ -1,19 +1,17 @@
 from filmFinder.models import *
 
-# import is the current userid and movieid
-# output is a list [review1, review2,...]
-# each element in list is a dict {userId:..., username:...}
+
+# import is the current userid, movieid
+# output is a list [{current_users_review}, [review1, review2,...]]
+# if the current user has no review for this film, return [None, [review1, review2,...]]
+# each review in list is a dict {userId:..., username:...}
 # it does not contain reviews whose owner is in the block list
 def get_review_datails(current_user_id, movieId):
-    block_users = []
-    if current_user_id:
-        block = BLOCKING.query.filter(BLOCKING.userid == current_user_id).all()
-        if block != [None]:
-            block_users = set(map(lambda x: x.blockid, block))
-        else:
-            block_users = []
+    block = BLOCKING.query.filter(BLOCKING.userid == current_user_id).all()
+    block_users = set(map(lambda x: x.blockid, block))
+
     reviews = RATINGS.query.filter(RATINGS.movieId == movieId).all()
-    output = []
+    output = [None, []]
     for review in reviews:
         if review.userId not in block_users:
             review_info = {}
@@ -25,24 +23,28 @@ def get_review_datails(current_user_id, movieId):
             review_info['rating'] = review.rating
             review_info['review'] = review.review
             review_info['id'] = review.index
-            output.append(review_info)
+            if review.userId != current_user_id:
+                output[1].append(review_info)
+            else:
+                output[0] = review_info
 
     return output
 
-# add or change review
+
+# add review
 # userId, movieId, rate is int
 # review is a string
-def add__review(userId, movieId, rating, review):
+def add_review(userId, movieId, rating, review):
     if RATINGS.query.filter(RATINGS.userId == userId).filter(RATINGS.movieId == movieId).first() != None:
-         flash('You have already written a review. Delete this to add a new one', category='danger')
+        flash('You have already written a review. Delete this to add a new one', category='danger')
         # RATINGS.query.filter(RATINGS.userId == userId).filter(RATINGS.movieId == movieId).update({'rating':rating, 'review':review})
     else:
         db.session.add(RATINGS(userId=userId, movieId=movieId, rating=rating, review=review))
-    db.session.commit()
+        db.session.commit()
+
 
 # delete review
 def delete_review(userId, movieId):
     user = RATINGS.query.filter(RATINGS.userId == userId).filter(RATINGS.movieId == movieId).first()
     db.session.delete(user)
     db.session.commit()
-
